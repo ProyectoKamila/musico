91b0c517aa232e0e6186186907b3c616e994ca9b
Jose Vivas <jose.vivas@proyectokamila.com>
2015-05-06 10:07:34 -0430

    Funciones bases

--- functions.php
+++ functions.php
@@ -1,3 +1,35 @@
 <?php
+//Cambior el logo de inicio de Sesion
+add_action('login_head', 'custom_login_logo');
+include('library/login-logo.php');
+//Funciones para crear post y Caetegory personalizados
+add_action('init', 'theme_custom_types');
+include('library/custom-post-type.php');
+//Texto de desarrollado por y el enlace a web de desarrollo
+add_filter('admin_footer_text', 'left_admin_footer_text_output');
+add_filter('update_footer', 'right_admin_footer_text_output', 11);
+include('library/copyright.php');
+//borrar dashboard y mensaje de actualizacion
+//add_action('wp_dashboard_setup', 'custom_dashboard_widgets');
+include('library/widget-delete.php');
+//Debug para las consultas
+include('library/debug.php');
+//opciones del themplate
+include('library/options.php');
+//generar las portadas
+include('library/portada.php');
+//funcion para extraer la url del thumbnail
+include('library/imgurl.php');
+//funcion para acortar cadenas
+include('library/limited-caracter.php');
+show_admin_bar( false );
 
-include 'library/debug.php';
+
+//Crear Post Personalizados
+function theme_custom_types() {
+
+    add_custom_post_type(array(
+        'type' => 'Anuncios',
+        'singular' => 'anuncio'
+    ));
+}
